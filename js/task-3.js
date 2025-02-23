3b8dccd6edb132367784975b01168a49f3f5f666
fokasiyy <foka123zate@gmail.con>
2025-02-23 13:09:05 +0200

    add markup header

--- js/task-3.js
+++ js/task-3.js
@@ -1,19 +1,12 @@
-function checkForSpam(message) {
-  const normalMessage = message.toLowerCase();
-  if (normalMessage.includes(`spam`)|| normalMessage.includes(`sale`)) {
-      return true
+function filterArray(numbers, value) {
+  const arr = [];
+  for (let n of numbers) {
+    if (n > value) arr.push(n);
   }
-  else {
-    return  false
-  }
-  }
-  
-  
-  
-  console.log(checkForSpam("Latest technology news")); // false
-  console.log(checkForSpam("JavaScript weekly newsletter")); // false
-  console.log(checkForSpam("Get best sale offers now!")); // true
-  console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-  console.log(checkForSpam("Trust me, this is not a spam message")); // true
-  console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-  console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+  return arr;
+};
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
